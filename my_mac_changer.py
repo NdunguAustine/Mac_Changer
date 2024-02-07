7ce995eb74b966afdf339699e0b176763cef55bb
NdunguAustine <ndunguaus@gmail.com>
2024-02-07 21:56:29 +0300

    saving subprocess

--- my_mac_changer.py
+++ my_mac_changer.py
@@ -17,9 +17,14 @@ def change_mac_address(user_interface, user_mac_address):
     subprocess.call(["ifconfig", user_interface, "up"])
 
 
+def control_new_mac(interface):
+    ifconfig = subprocess.check_output(["ifconfig", interface])
+    print(ifconfig)
+
 print("My MacChanger Started!")
-#Assigning arguments to function get_user_inputs
+# Assigning arguments to function get_user_inputs
 (user_input, arguments) = get_user_inputs()
 
-#passing the arguments to fuction change_mac_address
+# passing the arguments to fuction change_mac_address
 change_mac_address(user_input.interface, user_input.mac_address)
+control_new_mac(user_input.interface)
