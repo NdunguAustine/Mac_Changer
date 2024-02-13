831903d5d1f163a93c871157ac884257bb6ae34b
NdunguAustine <ndunguaus@gmail.com>
2024-02-13 10:40:19 +0300

    Tool Complete

--- my_mac_changer.py
+++ my_mac_changer.py
@@ -20,17 +20,23 @@ def change_mac_address(user_interface, user_mac_address):
 
 def control_new_mac(interface):
     ifconfig = subprocess.check_output(["ifconfig", interface])
-    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig)
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
 
     if new_mac:
         return new_mac.group(0)
     else:
         return None
 
+
 print("My MacChanger Started!")
 # Assigning arguments to function get_user_inputs
 (user_input, arguments) = get_user_inputs()
 
-# passing the arguments to fuction change_mac_address
+# passing the arguments to function change_mac_address
 change_mac_address(user_input.interface, user_input.mac_address)
-control_new_mac(user_input.interface)
+finalized_mac = control_new_mac(str(user_input.interface))
+
+if finalized_mac == user_input.mac_address:
+    print("Success")
+else:
+    print("Error!")
